fc1a42f488e6f1b6e805999c301842546eed1b88
Daniel <danielakindoyin2@gmail.com>
2026-08-28 14:10:59 +0100

    Production ready

--- database/db.js
+++ database/db.js
@@ -7,26 +7,18 @@ const isProduction = process.env.NODE_ENV === "production";
 const pool = new Pool(
     isProduction
         ? {
-              connectionString: process.env.DATABASE_URL,
-              ssl: {
-                  rejectUnauthorized: false
-              }
-          }
+            connectionString: process.env.DATABASE_URL,
+            ssl: {
+                rejectUnauthorized: false
+            }
+        }
         : {
-              user: process.env.DB_USER,
-              host: process.env.DB_HOST,
-              database: process.env.DB_NAME,
-              password: process.env.DB_PASSWORD,
-              port: process.env.DB_PORT
-          }
+            user: process.env.DB_USER,
+            host: process.env.DB_HOST,
+            database: process.env.DB_NAME,
+            password: process.env.DB_PASSWORD,
+            port: process.env.DB_PORT
+        }
 );
-
-pool.on("connect", () => {
-    console.log("PostgreSQL connection established");
-});
-
-pool.on("error", (error) => {
-    console.error("Unexpected PostgreSQL error:", error);
-});
 
 module.exports = pool;
